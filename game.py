20ac8f1973f1092696dff4b97a74481a4069d41b
Elizaveta Anikina <lizok.94.94@mail.ru>
2025-06-30 22:51:36 +0300

    intro pandas

--- game.py
+++ game.py
@@ -1,20 +1,5 @@
 import numpy as np
-
-number = np.random.randint(1, 101) # загадываем число
-count = 0
-
-while True:
-    count += 1
-    predict_number = int(input("Угадай число от 1 до 100"))
-
-    if predict_number > number:
-        print("Число должно быть меньше!")
-
-    elif predict_number < number:
-        print("Число должно быть больше!")
-
-    else:
-        print(f"Вы угадали число! Это число = {number}, за {count} попыток")
-        break # конец игры, выход из цикла5
-    
-
+a = np.int8(25)
+print(a)
+#np.iinfo(np.int64)
+np.iinfo(a)
